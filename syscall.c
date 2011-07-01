eb604dcbb5f2c3c6ae5ec7a82a8a95a0bb769fa3
Aaron Brown <abrown@cs.wisc.edu>
2011-06-30 22:10:16 -0700

    0 is not a valid syscall

--- syscall.c
+++ syscall.c
@@ -128,7 +128,7 @@ syscall(void)
   int num;
   
   num = proc->tf->eax;
-  if(num >= 0 && num < NELEM(syscalls) && syscalls[num])
+  if(num > 0 && num < NELEM(syscalls) && syscalls[num])
     proc->tf->eax = syscalls[num]();
   else {
     cprintf("%d %s: unknown sys call %d\n",
