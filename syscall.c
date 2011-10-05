fac9c8beca9b077c573982ae3a3150f7b9f2ac2e
Aaron Brown <this.aaron@gmail.com>
2011-10-05 14:26:12 -0700

    move syscall function declarations to syscall.h so compiler can catch definitinos that don't match

--- syscall.c
+++ syscall.c
@@ -76,27 +76,8 @@ argstr(int n, char **pp)
   return fetchstr(proc, addr, pp);
 }
 
-extern int sys_chdir(void);
-extern int sys_close(void);
-extern int sys_dup(void);
-extern int sys_exec(void);
-extern int sys_exit(void);
-extern int sys_fork(void);
-extern int sys_fstat(void);
-extern int sys_getpid(void);
-extern int sys_kill(void);
-extern int sys_link(void);
-extern int sys_mkdir(void);
-extern int sys_mknod(void);
-extern int sys_open(void);
-extern int sys_pipe(void);
-extern int sys_read(void);
-extern int sys_sbrk(void);
-extern int sys_sleep(void);
-extern int sys_unlink(void);
-extern int sys_wait(void);
-extern int sys_write(void);
-extern int sys_uptime(void);
+// syscall function declarations moved to syscall.h so compiler
+// can catch definitions that don't match
 
 // array of function pointers to handlers for all the syscalls
 static int (*syscalls[])(void) = {
